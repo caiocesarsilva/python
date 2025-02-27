fa5a48b728aaea3b957eec22ee567deea466e0b5
Caio <caio.eng.silva@gmail.com>
2025-02-26 22:15:41 -0300

    25_02_26_1

--- python_cookbook/01_data_structures_and_algorithms/03_keeping_the_last_n_items.py
+++ python_cookbook/01_data_structures_and_algorithms/03_keeping_the_last_n_items.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def search(lines, pattern, history=5)
+def search(lines,pattern,history=5):
     previous_lines = deque(maxlen = history)
     for line in lines:
         if pattern in line:
@@ -10,4 +10,8 @@ def search(lines, pattern, history=5)
 #Exemple use on a file
 if __name__ == '__main__':
     with open('somefile.txt') as f:
-        for line, prevlines in search()
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end = '')
+            print(line, end = '')
+            print('-'*20)
