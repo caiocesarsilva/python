dacc550fb8e46298d6b8d8d5dd7aeb0ae97a72b4
caioengcesar <caio.eng.silva@gmail.com>
2025-03-26 20:44:24 -0300

    25_03_26

--- python_data_science_handbook/04_understanding_data_types_in_python/01_intro.py
+++ python_data_science_handbook/04_understanding_data_types_in_python/01_intro.py
@@ -9,3 +9,16 @@ x = 4
 print(x)
 x = 'four'
 print(x)
+
+#######################################################################
+
+L = list(range(10))
+print(L)
+print(type(L[0]))
+
+L2 = list(str(c) for c in L)
+print(L2)
+print(type(L2[0]))
+
+L3 = [True, '2', 3.0, 4]
+print([type(item) for item in L3])
